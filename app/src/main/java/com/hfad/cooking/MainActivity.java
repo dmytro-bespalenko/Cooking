60feb0fcff63f7b941d80ae32a4ac06fd23e3b0e
Dmytro1602 <Dmytro1602@users.noreply.github.com>
2020-10-15 11:16:10 +0300

    Editable

--- app/src/main/java/com/hfad/cooking/MainActivity.java
+++ app/src/main/java/com/hfad/cooking/MainActivity.java
@@ -3,6 +3,10 @@ package com.hfad.cooking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.editText);
+
+        final TextView textView = findViewById(R.id.showText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String result = s.toString();
+
+                textView.setText(result);
+            }
+        });
+
+
     }
 }
